bf9703059df897c9a71863933b29eb088ac4a6bb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-06 11:25:06 +0000

    Sat Jan 06 11:25:06 GMT 2018

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -5,10 +5,34 @@
  */
 package befaster.solutions;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  *
  * @author George Cristian
  */
 public class FizzBuzzTest {
+    
+    @Test
+    public void testFizzMultipleThree() {
+        assertThat(FizzBuzz.fizzBuzz(9), equalTo("fizz"));
+    }
+    
+    @Test
+    public void testFizzMultipleFive() {
+        assertThat(FizzBuzz.fizzBuzz(25), equalTo("buzz"));
+    }
+    
+    @Test
+    public void testFizzMultipleThreeFive() {
+        assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+    }
+    
+    @Test
+    public void testFizzNotMultipleThreeFive() {
+        assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
+    }
     
 }
